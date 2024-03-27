78914b90bf07a5ae74058741b91c691bd974ac46
Sasa Nedic <svetasmak0@gmail.com>
2024-03-27 20:15:30 +0100

    Change code in Header component

--- Book-library/src/components/Header.tsx
+++ Book-library/src/components/Header.tsx
@@ -1,15 +1,20 @@
-function Header({handleClick}) {
-    return (
-      <div className="text__div">
-        <h1 className="heading__primary">
-          <span>📚</span>Book library<span>📚</span>
-        </h1>
-        <p className="text__paragraph">
-          Use the button below to add to your library and watch your collection
-          grow
-        </p>
-        <button className="btn button__new__book" onClick={handleClick}>New Book</button>
-      </div>
-    );
-  }
-  export default Header;
+type HeaderProps = {
+  handleClick: () => void;
+}
+
+const Header = ({ handleClick }:HeaderProps) => {
+  return (
+    <div className="text__div">
+      <h1 className="heading__primary">
+        <span>📚</span>Book library<span>📚</span>
+      </h1>
+      <p className="text__paragraph">
+        Use the button below to add to your library and watch your collection
+        grow
+      </p>
+      <button className="btn button__new__book" onClick={handleClick}>New Book</button>
+    </div>
+  );
+}
+
+export default Header;
