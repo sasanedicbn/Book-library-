1f92e03c08d275210e2685bae15298103e765dc5
Sasa Nedic <svetasmak0@gmail.com>
2024-03-27 14:17:36 +0100

    Create BookList component

--- Book-library/src/App.tsx
+++ Book-library/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Header from './components/Header'
 import BookForm from './components/BookForm'
+import BookList from './components/BookList'
+
 function App() {
 
 
@@ -8,6 +10,7 @@ function App() {
     <>
      <Header/>
      <BookForm/>
+     <BookList/>
 
     </>
   )
---
+++ Book-library/src/components/BookList.tsx
@@ -0,0 +1,10 @@
+function BookList() {
+    return (
+      <div className="list__div">
+        <ul className="ul__list">
+          {/* ovdje trebam ubaciti knjigee */}
+        </ul>
+      </div>
+    );
+  }
+  export default BookList;
